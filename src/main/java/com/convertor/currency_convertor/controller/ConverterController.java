e4f644b3ef6ed9e291afe500c0d3fc5ea0596dce
nuwanthi1 <nuwanthi@hsenidlanka.com>
2025-04-15 16:13:40 +0530

    Revert "Added basic file structure"
    This reverts commit 180a27a3dd30bea3d4b9304a5f721bb61c1dd912.

--- src/main/java/com/convertor/currency_convertor/controller/ConverterController.java
+++
@@ -1,4 +0,0 @@
-package com.convertor.currency_convertor.controller;
-
-public class ConverterController {
-}
--- src/main/java/com/convertor/currency_convertor/repository/ConverterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.convertor.currency_convertor.repository;
-
-public class ConverterRepository {
-
-    // FOR FUTURE USE
-
-}
